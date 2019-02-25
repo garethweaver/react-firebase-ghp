3752f0b82d004f3ab710701458bb1054c4f9e39c
garethweaver <reg@garethweaver.com>
2019-02-25 17:56:46 +0000

    chore(remove react helmet, update manifest)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import {Helmet} from 'react-helmet'
 import {withFirebase} from './firebase'
 import Main from './components/main/main'
 import AuthLogin from './components/auth/auth-login'
@@ -25,17 +24,6 @@ class App extends Component {
   render() {
     return (
       <div className="App page-wrap">
-        <Helmet>
-          <title>Kortical Todos</title>
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:400"
-            rel="stylesheet" />
-          <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-            integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-            crossorigin="anonymous" />
-        </Helmet>
         {
           this.state.authUser
           ? <Main />
